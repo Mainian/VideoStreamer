5ecba786185c7797f8509dac21f7d8ea73cea414
Mainian <k032492@gmail.com>
2014-10-26 17:59:38 -0400

    Video Streamer

---
+++ VideoStreamer/Controllers/VideoStreamController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace VideoStreamer.Controllers
+{
+    public class VideoStreamController : ApiController
+    {
+        public HttpResponseMessage Get(string filepath, string ext)
+        {
+            var video = new VideoStreamer.Models.VideoStream(filepath, ext);
+
+            var response = Request.CreateResponse();
+            response.Content = new PushStreamContent(video.WriteToStream, new System.Net.Http.Headers.MediaTypeHeaderValue("video/" + ext));
+
+            return response;
+        }
+    }
+}

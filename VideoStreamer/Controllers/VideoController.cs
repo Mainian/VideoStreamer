79a45b08c50b30d0f448600a45c3b143822673a7
Mainian <k032492@gmail.com>
2014-10-29 17:50:05 -0400

    Broken Stuff

--- VideoStreamer/Controllers/VideoController.cs
+++ VideoStreamer/Controllers/VideoController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
+using System.Web.Mvc;
+using VideoStreamer.CustomResult;
 
 namespace VideoStreamer.Controllers
 {
-    public class VideoController : ApiController
+    public class VideoController : Controller
     {
         //public HttpResponseMessage Get(string filename, string ext)
         //{
         //    var response = Request.CreateResponse();
         //    response.Content = PushStreamContent(
         //}
+
+        public ActionResult Index(string filepath, string name)
+        {
+            return View(new VideoResult(filepath, name));
+        }
     }
 }
---
+++ VideoStreamer/CustomResult/VideoResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace VideoStreamer.CustomResult
+{
+    public class VideoResult : ActionResult
+    {
+        private string _filepath;
+        private string _name;
+
+        public VideoResult(string filepath, string name)
+        {
+            _filepath = filepath;
+            _name = name;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            //use filepath instead of HostingEnviornment
+
+            //Change me
+            //var videoFilePath = HostingEnvironment.MapPath("~/VideoFile/Win8.mp4");
+
+            //header information
+            context.HttpContext.Response.AddHeader("Content-Disposition", "attachment;filename="+_name);
+            var file = new FileInfo(_filepath);
+            if (file.Exists)
+            {
+                var stream = file.OpenRead();
+                var bytesinfile = new byte[stream.Length];
+                stream.Read(bytesinfile, 0, (int)file.Length);
+                context.HttpContext.Response.BinaryWrite(bytesinfile);
+            }
+        }
+    }
+}
